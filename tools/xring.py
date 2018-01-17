c78e54123e8687b952d401e7dedccb6188fdd92d
Your Name <you@example.com>
2018-01-17 17:22:44 +0800

    improve xring.py

--- tools/xring.py
+++ tools/xring.py
@@ -29,26 +29,44 @@ if __name__ == "__main__":
     logfile = 'xring-report.txt' 
     args = parser.parse_args()
     command = args.command[0]
+    max_num_gpus = args.max_num_gpus
 
     if command == 'record':
         os.system("echo XRING TEST > "+logfile)  
-        for i in range(1,8):
+        for i in range(1,max_num_gpus):
             print("Test xring for GPUx%d" % (i+1) )
             os.system("sofa stat  python tf_cnn_benchmarks.py --model=vgg16 --batch_size=64 --variable_update=replicated --num_gpus=%d --local_parameter_device=gpu --num_batches=10 --all_reduce_spec=xring 1>> %s " % ( i+1, logfile ) )
         
     if command == 'report':
-        traffics = []
+        total_traffic = []
+        total_h2d_traffic = []
+        total_d2h_traffic = []
+        total_p2p_traffic = []
         with open(logfile) as f:
             lines = f.readlines()
-            n_gpus = 0
             for line in lines:
                 if line.find("MeasuredTotalTraffic") != -1 :
                     traffic = int(float(line.split()[2])) 
-                    n_gpus = n_gpus + 1
-                    print("GPUx%d : %d" % ( n_gpus, traffic) )
-                    traffics.append(traffic)
+                    total_traffic.append(traffic)
+                if line.find("MeasuredTotalH2DTraffic") != -1 :
+                    traffic = int(float(line.split()[2])) 
+                    total_h2d_traffic.append(traffic)
+                if line.find("MeasuredTotalD2HTraffic") != -1 :
+                    traffic = int(float(line.split()[2])) 
+                    total_d2h_traffic.append(traffic)
+                if line.find("MeasuredTotalP2PTraffic") != -1 :
+                    traffic = int(float(line.split()[2])) 
+                    total_p2p_traffic.append(traffic)
+
         with open("xring.csv", "w") as outcsv:
-            writer = csv.writer(outcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+            writer = csv.writer(outcsv)
             writer.writerow(['GPUx2', 'GPUx3', 'GPUx4',  'GPUx5', 'GPUx6', 'GPUx7', 'GPUx8',])
-            writer.writerow(traffics)        
-        print(traffics) 
+            writer.writerow(total_traffic)        
+            writer.writerow(total_h2d_traffic)        
+            writer.writerow(total_d2h_traffic)        
+            writer.writerow(total_p2p_traffic)        
+        print('Total Traffic, H2D, D2H, P2P (MB)')
+        print(total_traffic)
+        print(total_h2d_traffic) 
+        print(total_d2h_traffic) 
+        print(total_p2p_traffic) 
