cb3e9caaf51f96feb0e4024c9b23a70915a6c255
Your Name <you@example.com>
2018-03-05 15:42:47 +0800

    update run_all for training with real data

--- tools/runall_real_data.sh
+++ tools/runall_real_data.sh
@@ -3,7 +3,7 @@ ps_port=$(shuf -i 50000-65535 -n 1)
 wk_port=$(shuf -i 58000-65535 -n 1)
 ps_servers=""
 wk_servers=""
-first_node=0
+first_node=1
 last_node=1
 num_nodes=`expr $last_node - $first_node + 1`
 for (( i=$first_node ; i <= $last_node ; i++ ))
@@ -26,27 +26,30 @@ do
 	echo "Lauch Node"$nid
 	ssh node$nid hostname 	
 
-	#ssh node$nid \
-	#cd $HOME/tensorflow-models/research/inception &&  \
-	#bazel-bin/inception/imagenet_distributed_train \
-	#--batch_size=16 \
-	#--data_dir=$HOME/ImageNetData/ImageNet_TFRecord \
-	#--job_name='worker' \
-	#--task_id=$i \
-	#--per_process_gpu_memory_fraction=0.45 \
-	#--ps_hosts=$ps_servers \
-	#--worker_hosts=$wk_servers &
-	#
-	#ssh node$nid \
-	#cd $HOME/tensorflow-models/research/inception &&  \
-	#bazel-bin/inception/imagenet_distributed_train \
-	#--batch_size=16 \
-	#--data_dir=$HOME/ImageNetData/ImageNet_TFRecord \
-	#--job_name='ps' \
-	#--task_id=$i \
-	#--per_process_gpu_memory_fraction=0.45 \
-	#--ps_hosts=$ps_servers \
-	#--worker_hosts=$wk_servers &
+	ssh node$nid \
+	"cd tensorflow-models/research/inception &&  \
+	bazel-bin/inception/imagenet_distributed_train \
+	--input_queue_memory_factor=4 \
+	--batch_size=16 \
+	--data_dir=$HOME/ImageNetData/ImageNet_TFRecord \
+	--job_name='worker' \
+	--task_id=$i \
+	--ps_hosts=$ps_servers \
+	--train_dir=/tmp/imagenet_train \
+	--worker_hosts=$wk_servers" &
+
+	
+	ssh node$nid \
+	"cd tensorflow-models/research/inception &&  \
+	bazel-bin/inception/imagenet_distributed_train \
+	--input_queue_memory_factor=4 \
+	--batch_size=16 \
+	--data_dir=$HOME/ImageNetData/ImageNet_TFRecord \
+	--job_name='ps' \
+	--task_id=$i \
+	--ps_hosts=$ps_servers \
+	--train_dir=/tmp/imagenet_train \
+	--worker_hosts=$wk_servers" &
 done
 
 
