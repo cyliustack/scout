94bbfde81a3824fd13f5d04b53a68b9c995919ca
Your Name <you@example.com>
2018-04-12 16:15:25 +0800

    update killing

--- tools/killall.sh
+++ tools/killall.sh
@@ -3,5 +3,6 @@ num_nodes=2
 for (( i=0 ; i<$num_nodes ; i++  ))
 do
 	echo "Kill CUDA process on node"$i
+	ssh node$i "source ~/scout/tools/killsofa.sh ;"
 	ssh node$i "source ~/scout/tools/killtf.sh ; sleep 1 &&  nvidia-smi"
 done
---
+++ tools/killsofa.sh
@@ -0,0 +1,1 @@
+ps aux | grep sofa | awk {'print $2'} | xargs kill -9
